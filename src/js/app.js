bfdc64fdaa52122c8fed9e98496e9e1f9e2eb2c7
pauchok-sk <pauchoooooook@gmail.com>
2025-07-23 00:16:22 +0300

    s-about dev

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import "../scss/style.scss";
+import burger from "./files/burger.js";
+import headerScroll from "./files/headerScroll.js";
+import spoller from "./files/spoller.js";
+
+spoller();
+burger();
+headerScroll();
